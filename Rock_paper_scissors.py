bd99716846eff6b1cee68fe22764f6e41d0194d1
AdamCedrowicz <adamcedrowicz@gmail.com>
2022-06-12 13:50:10 +0200

    Feat: In this commit has been added new feature which is protecting by incorrect input

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,12 +1,22 @@
 #library
 import random as r
 #game
+possible_input = ['p','r','s']
+
 def game():
-    opponent = r.choice(['p','r','s'])
+    opponent = r.choice(possible_input)
     user = input("'p' - paper, 'r' - rock, 's' - scissors."\
         " What's your choice?"\
-            " ")     
-
+            " ")  
+       
+    while user.lower() not in possible_input:
+        user = input("Incorrect input, you need choose 'p', 'r' or 's'. Fill it once again please. ")
+    else:
+        pass
+    
+    user = user.lower()  
+    print(user)
+    print(opponent)
     if user == opponent:
        return " It's a draw"
 
@@ -19,5 +29,4 @@ def you_won (user,opponent):
     if (user == 'p' and opponent == 'r') or (user == 'r' and opponent == 's') or \
        (user == 's' and opponent == 'p'):
        return True
-
 print(game())
